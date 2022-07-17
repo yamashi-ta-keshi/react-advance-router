9a3b0cfedb03b54d3beaf58458d482c4e0508111
yamashi-ta-keshi <59694038+yamashi-ta-keshi@users.noreply.github.com>
2022-07-18 00:59:15 +0900

    add state usage on page1

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="/page1/detail1">Page1Detail1</Link>
+      <Link to={{ pathname: "/page1/detail1", state: arr }}>Page1Detail1</Link>
       <br />
       <Link to="/page1/detail2">Page1Detail2</Link>
     </div>
--- src/Page1Detail1.jsx
+++ src/Page1Detail1.jsx
@@ -1,3 +1,8 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1Detail1 = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return <h1>Page1Detail1</h1>;
 };
